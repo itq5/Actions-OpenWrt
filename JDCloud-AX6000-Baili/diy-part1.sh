beadbc1fddcda2ad6b9824b5527985cd316f5b0b
Cloud 1024 <42521985+itq5@users.noreply.github.com>
2024-06-06 02:15:33 +0800

    Update diy-part1.sh

--- JDCloud-AX6000-Baili/diy-part1.sh
+++ JDCloud-AX6000-Baili/diy-part1.sh
@@ -21,3 +21,5 @@ git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
