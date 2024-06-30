a6618b8c9f6f2f873bf5fd0f068aee1a8ad72cc8
Cloud 1024 <42521985+itq5@users.noreply.github.com>
2024-06-30 13:09:33 +0800

    Update diy-part1.sh

--- JDCloud-AX6000-Baili/diy-part1.sh
+++ JDCloud-AX6000-Baili/diy-part1.sh
@@ -23,3 +23,4 @@ echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.defa
 echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master' >> feeds.conf.default
